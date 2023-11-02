6923e0447831de06eb7c4c06eb33e706a8db450f
Mayur-Mori <morimayur13102001@gmail.com>
2023-11-02 10:29:31 +0530

    array

--- Module 2.3/Module2.3_Str_rev_using_recursion.c
+++ Module 2.3/Module2.3_Str_rev_using_recursion.c
@@ -8,16 +8,25 @@ main()
 	printf("\n\n Enter the string :");
 	gets(str);
 	 printf("\n %s",str);
-	
-	
-	
+	 	
 	len=strlen(str);
 	printf("\n\n %d \n\n",len);
-	for(i=len-1;i>=0;i--)
+	i=len-1;
+	reverse:
 	{
 		str1[j]=str[i];
 		printf("%c",str1[j]);
 		j++;
+		i--;
 	}
-	printf("\n %s",str1);
+	
+	
+	if(i<len && i>=0)
+	{
+		goto reverse;
+	}
+	
+	
+	
+	
 }
